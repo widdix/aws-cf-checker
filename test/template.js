3eecb4e52aecf52916f41cea29be0869b9c564a8
Michael Wittig <post@michaelwittig.info>
2016-06-08 16:56:51 +0200

    catch missing checks

--- test/template.js
+++ test/template.js
@@ -4,6 +4,19 @@ var checker = require("../index.js");
 var assert = require("assert-plus");
 
 describe("templates", function() {
+  describe("missing check", function() {
+    it("check", function(done) {
+      checker.checkFile("./test/templates/template1.json", {
+        "missing": true
+      }, function(err, findings) {
+        if (err) {
+          done();
+        } else {
+          assert.fail();
+        }
+      });
+    });
+  });
   describe("template0", function() {
     it("check", function(done) {
       checker.checkFile("./test/templates/template0.json", {
